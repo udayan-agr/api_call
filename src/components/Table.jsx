3911bb8c46c7a0cf341d6957df61f5ee7851753f
Udayan Agrawal <udayan.agrawal@paytm.com>
2020-09-03 13:09:16 +0530

    changed to class components

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,37 +1,72 @@
 import React from "react";
 
-function TableData(props) {
-	return (
-		<tr>
-			<td>{props.row.name}</td>
-			<td>{props.row.profession}</td>
-			<td>{props.row.actor}</td>
-			<td>{props.row.rating}</td>
-		</tr>
-	);
+class TableData extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<tr>
+				<td>{this.props.row.profession}</td>
+				<td>{this.props.row.actor}</td>
+				<td>{this.props.row.name}</td>
+				<td>{this.props.row.rating}</td>
+			</tr>
+		);
+	}
 }
 
-function Table(props) {
-	return (
-		<div className="table">
-			<h1>Movie Characters</h1>
-			<table style={{ margin: "auto", padding: "1.5rem" }}>
-				<thead>
-					<tr>
-						<th>Character Name</th>
-						<th>Profession</th>
-						<th>Actor</th>
-						<th>Rating</th>
-					</tr>
-				</thead>
-				<tbody>
-					{props.tableData.map((ele, index) => {
-						return <TableData key={index} row={ele}></TableData>;
-					})}
-				</tbody>
-			</table>
-		</div>
-	);
+class Table extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="table">
+				<h1>Movie Characters</h1>
+				<table style={{ margin: "auto", padding: "1.5rem" }}>
+					<thead>
+						<tr>
+							<th>Character Name</th>
+							<th>Profession</th>
+							<th>Actor</th>
+							<th>Rating</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.props.tableData.map((ele, index) => {
+							return <TableData key={index} row={ele}></TableData>;
+						})}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
+
+// function Table(props) {
+// 	return (
+// 		<div className="table">
+// 			<h1>Movie Characters</h1>
+// 			<table style={{ margin: "auto", padding: "1.5rem" }}>
+// 				<thead>
+// 					<tr>
+// 						<th>Character Name</th>
+// 						<th>Profession</th>
+// 						<th>Actor</th>
+// 						<th>Rating</th>
+// 					</tr>
+// 				</thead>
+// 				<tbody>
+// 					{props.tableData.map((ele, index) => {
+// 						return <TableData key={index} row={ele}></TableData>;
+// 					})}
+// 				</tbody>
+// 			</table>
+// 		</div>
+// 	);
+// }
 
 export default Table;
